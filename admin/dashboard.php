156f989f1d7fd8e5cb74d86fb6c795f87a6fafb4
varelantony <varelantony@itbss.ac.id>
2026-06-12 22:50:29 +0700

    mengedit menu manajemen pendaftaran di dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,4 +6,6 @@ echo "welcome, ".$username;
 <br>
 <a href="courses/index.php">manajemen kelas</a>
 <br>
+<a href="registrations/index.php">manajemen pendaftaran</a>
+<br>
 <a href="logout.php">logout</a>
